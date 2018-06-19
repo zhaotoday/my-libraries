4368b512a5d51a9e888e4318d91558191d5d25d1
zhaojintian <729234283@qq.com>
2018-06-19 20:59:07 +0800

    优化

--- mp-client/src/utils/index.js
+++ mp-client/src/utils/index.js
@@ -3,11 +3,13 @@ import Storage from './storage'
 import currentPages from './currentPages'
 import url from './url'
 import page from './page'
+import time from './time'
 
 export default {
   consts,
   Storage,
   currentPages,
   url,
-  page
+  page,
+  time
 }
