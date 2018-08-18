9e90d5be5022234120e927e96c048adbfd82443c
zhaojintian <729234283@qq.com>
2018-08-18 15:49:08 +0800

    优化

---
+++ env/src/index.js
@@ -0,0 +1,41 @@
+const ENVs = {
+  DEV: 'DEV',
+  TEST: 'TEST',
+  BETA: 'BETA',
+  PROD: 'PROD'
+}
+
+export default {
+  _get () {
+    const hostname = window.location.hostname
+
+    switch (hostname) {
+      case '127.0.0.1':
+      case 'localhost':
+        return ENVs.DEV
+      default:
+        if (/\.test\.liruan\.cn$/.test(hostname)) {
+          return ENVs.TEST
+        }
+        if (/\.beta\.liruan\.cn$/.test(hostname)) {
+          return ENVs.BETA
+        }
+        if (/\.prod\.liruan\.cn$/.test(hostname)) {
+          return ENVs.PROD
+        }
+        return ENVs.PROD
+    }
+  },
+  isDev () {
+    return this._get() === ENVs.DEV
+  },
+  isTest () {
+    return this._get() === ENVs.TEST
+  },
+  isBeta () {
+    return this._get() === ENVs.BETA
+  },
+  isProd () {
+    return this._get() === ENVs.PROD
+  }
+}
