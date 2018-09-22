d4e6668aaa678f8c01b768e55c6c949642162a5f
zhaojintian <729234283@qq.com>
2018-09-22 23:57:49 +0800

    update

--- mp-client/src/utils/time.js
+++ mp-client/src/utils/time.js
@@ -3,6 +3,9 @@ import dayjs from 'dayjs'
 const DATE_FORMATTER = 'YYYY-MM-DD'
 
 export default {
+  format (time, formatter) {
+    return dayjs(time).format(formatter)
+  },
   getDate (time) {
     return dayjs(time).format(DATE_FORMATTER)
   }
