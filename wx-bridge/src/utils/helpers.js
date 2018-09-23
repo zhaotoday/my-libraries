4a535e96dae084f503954e3a0dcf1674c98254b3
zhaojintian <729234283@qq.com>
2018-09-23 09:56:07 +0800

    update

--- wx-bridge/src/utils/helpers.js
+++ wx-bridge/src/utils/helpers.js
@@ -3,7 +3,7 @@ import Promise from 'es6-promise'
 export default {
   promisify (fn, options) {
     return new Promise((resolve, reject) => {
-      wx[fn](Object.assign(options, {
+      wx[fn](Object.assign({}, options, {
         success: resolve,
         fail: reject
       }))
