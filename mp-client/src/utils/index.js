4346d6779f478e7eeaba4cab9b79af1320b327c5
zhaojintian <729234283@qq.com>
2019-03-27 21:22:19 +0800

    优化

--- mp-client/src/utils/index.js
+++ mp-client/src/utils/index.js
@@ -2,12 +2,10 @@ import Storage from './storage'
 import url from './url'
 import page from './page'
 import time from './time'
-import payment from './payment'
 
 export default {
   Storage,
   url,
   page,
-  time,
-  payment
+  time
 }
