d4d34eae97710a83a6d64c66d5f46df5e6c92ca2
zhaojintian <729234283@qq.com>
2019-10-10 23:39:46 +0800

    update

--- wx-server-api/cgi/index.js
+++ wx-server-api/cgi/index.js
@@ -1,6 +1,7 @@
 module.exports = config => ({
   ...require('./base')(config),
   message: {
+    template: require('./message/template/index')(config),
     wxopen: {
       template: require('./message/wxopen/template')(config)
     }
--- wx-server-api/cgi/message/template/index.js
+++ wx-server-api/cgi/message/template/index.js
@@ -6,25 +6,23 @@ module.exports = config => {
       {
         touser,
         template_id,
-        page,
-        form_id,
-        data,
-        emphasis_keyword
+        url = '',
+        topcolor = '#FF0000',
+        data
       }
     ) {
       const cgi = require('../../../base')(config)
       const { access_token } = await cgi.getAccessToken()
       const res = await axios.request({
         method: 'POST',
-        url: 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send',
+        url: 'https://api.weixin.qq.com/cgi-bin/message/template/send',
         params: { access_token },
         data: {
           touser,
           template_id,
-          page,
-          form_id,
-          data,
-          emphasis_keyword
+          url,
+          topcolor,
+          data
         }
       })
 
