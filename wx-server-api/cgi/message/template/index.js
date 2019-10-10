447f66f868f68e7b2506dd13065d17656cb9e2b8
zhaojintian <729234283@qq.com>
2019-10-10 23:32:49 +0800

    update

---
+++ wx-server-api/cgi/message/template/index.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+
+module.exports = config => {
+  return {
+    async send (
+      {
+        touser,
+        template_id,
+        page,
+        form_id,
+        data,
+        emphasis_keyword
+      }
+    ) {
+      const cgi = require('../../../base')(config)
+      const { access_token } = await cgi.getAccessToken()
+      const res = await axios.request({
+        method: 'POST',
+        url: 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send',
+        params: { access_token },
+        data: {
+          touser,
+          template_id,
+          page,
+          form_id,
+          data,
+          emphasis_keyword
+        }
+      })
+
+      return res.data
+    }
+  }
+}
