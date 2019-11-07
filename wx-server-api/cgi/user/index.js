3eb2af7459d517a7eae0a4621c80c25653d35d8e
zhaojintian <729234283@qq.com>
2019-11-08 07:25:06 +0800

    update

--- wx-server-api/cgi/user/index.js
+++ wx-server-api/cgi/user/index.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 
 module.exports = config => {
   return {
-    async getInfo ({ openId }) {
+    async getInfo ({ code, openId }) {
       const sns = require('../../sns')(config)
-      const { access_token } = await sns.oauth2.getAccessToken()
+      const { access_token } = await sns.oauth2.getAccessToken({ code })
       const { data } = await axios.request({
         method: 'GET',
         url: 'https://api.weixin.qq.com/cgi-bin/user/info',
