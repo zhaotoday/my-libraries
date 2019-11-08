eaea1cf7547453ea45fb1092714d1a347e1abb91
zhaojintian <729234283@qq.com>
2019-11-08 09:40:40 +0800

    update

--- wx-server-api/cgi/user/index.js
+++ wx-server-api/cgi/user/index.js
@@ -2,14 +2,12 @@ const axios = require('axios')
 
 module.exports = config => {
   return {
-    async getInfo ({ code, openId }) {
-      const sns = require('../../sns')(config)
-      const { access_token } = await sns.oauth2.getAccessToken({ code })
+    async getInfo ({ accessToken, openId }) {
       const { data } = await axios.request({
         method: 'GET',
         url: 'https://api.weixin.qq.com/cgi-bin/user/info',
         params: {
-          access_token,
+          access_token: accessToken,
           openid: openId,
           lang: 'zh_CN'
         }
--- wx-server-api/sns/base.js
+++ wx-server-api/sns/base.js
@@ -2,14 +2,15 @@ const axios = require('axios')
 
 module.exports = config => {
   return {
-    async getUserInfo ({ code }) {
-      const oauth2 = require('./oauth2')(config)
-      const { access_token, openid } = await oauth2.getAccessToken({ code })
-
+    async getUserInfo ({ accessToken, openId }) {
       const { data } = await axios.request({
         method: 'GET',
         url: 'https://api.weixin.qq.com/sns/userinfo',
-        params: { access_token, openid, lang: 'zh_CN' }
+        params: {
+          access_token: accessToken,
+          openid: openId,
+          lang: 'zh_CN'
+        }
       })
 
       return data
