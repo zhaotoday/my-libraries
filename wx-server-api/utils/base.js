3c918776f9af56a6b1f3020bf4b5f2b68361f0f1
zhaojintian <729234283@qq.com>
2020-02-12 23:14:34 +0800

    update

--- wx-server-api/utils/base.js
+++ wx-server-api/utils/base.js
@@ -3,7 +3,7 @@ const WXBizDataCrypt = require('../lib/WXBizDataCrypt')
 module.exports = config => {
   return {
     getDecryptedData ({ sessionKey, encryptedData, iv }) {
-      return new WXBizDataCrypt(config.oa.appId, sessionKey).decryptData(
+      return new WXBizDataCrypt(config.mp.appId, sessionKey).decryptData(
         encryptedData, iv)
     }
   }
