0dd34664a529da7e35e5ae93ca0f0ef8310eae0e
729234283 <729234283@qq.com>
2020-12-30 15:49:44 +0800

    update

--- vue-tree/utils/rest.js
+++ vue-tree/utils/rest.js
@@ -1,16 +1,17 @@
+import { useRouter } from "vue-router";
 import REST from "jt-rest";
-import ViewUI from "view-design";
-import dayjs from "dayjs";
+import { ElMessage } from "element-plus";
 
 export default class extends REST {
-  _toString(obj = {}) {
-    let ret = {};
-    let types = [];
+  showLoading() {}
 
-    const { dateRange, ...restObj } = obj;
+  hideLoading() {}
 
-    Object.keys(restObj).forEach(v => {
-      types = Object.keys(obj[v]);
+  toString(obj = {}) {
+    const ret = {};
+
+    Object.keys(obj).forEach(v => {
+      const types = Object.keys(obj[v]);
 
       if (types.length) {
         ret[v] = {};
@@ -39,28 +40,11 @@ export default class extends REST {
     return JSON.stringify(ret);
   }
 
-  _filter(query) {
-    if (query.where && query.where.dateRange && query.where.dateRange.$eq) {
-      const [date1 = "", date2 = ""] = query.where.dateRange.$eq;
-
-      query.where.createdAt = {};
-
-      if (date1) {
-        query.where.createdAt.$gt = dayjs(date1).startOf("day").$d;
-      }
-      if (date2) {
-        query.where.createdAt.$lt = dayjs(date2).endOf("day").$d;
-      }
-    }
-  }
-
   request(
     method = "GET",
     { id, query = {}, body = {}, showLoading = false, showError = true }
   ) {
-    this._filter(query);
-
-    query.where = this._toString(query.where);
+    query.where = this.toString(query.where);
 
     if (query.include) {
       query.include = JSON.stringify(query.include);
@@ -78,27 +62,27 @@ export default class extends REST {
       query._ = new Date().getTime();
     }
 
-    showLoading && ViewUI.Spin.show();
+    showLoading && this.showLoading();
 
     return new Promise((resolve, reject) => {
       super
         .request(method, { id, query, body })
         .then(res => {
-          showLoading && ViewUI.Spin.hide();
+          showLoading && this.hideLoading();
           resolve(res.data);
         })
         .catch(({ response: res }) => {
-          showLoading && ViewUI.Spin.hide();
+          showLoading && this.hideLoading();
 
           if (res.status === 401) {
-            ViewUI.Message.error("登入过期，请重新登入");
-            window.location.href = "index.html#/logout";
+            ElMessage.error("登入过期，请重新登入");
+            useRouter().push("/logout");
           } else {
             if (showError) {
               if (res.status === 500) {
-                ViewUI.Message.error("服务器出错");
+                ElMessage.error("服务器出错");
               } else {
-                ViewUI.Message.error(res.data.error.message);
+                ElMessage.error(res.data.error.message);
               }
             } else {
               reject(res.data.error);
