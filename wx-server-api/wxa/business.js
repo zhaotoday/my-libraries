8084e3449c76ff42b1c82c2b2f733b58c1969cab
zhaojintian <729234283@qq.com>
2022-02-20 11:19:48 +0800

    u

--- wx-server-api/wxa/business.js
+++ wx-server-api/wxa/business.js
@@ -2,11 +2,12 @@ const axios = require("axios");
 
 module.exports = () => {
   return {
-    async getUserPhoneNumber({ accessToken }) {
+    async getPhoneNumber({ accessToken, code }) {
       const { data } = await axios.request({
         method: "POST",
         url: "https://api.weixin.qq.com/wxa/business/getuserphonenumber",
         params: { access_token: accessToken },
+        data: { code },
       });
 
       return data;
