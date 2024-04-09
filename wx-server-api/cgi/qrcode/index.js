edfb6c89335a8497f131a8f3019c12a07f0661df
zhaojintian <729234283@qq.com>
2024-04-09 20:52:36 +0800

    update

--- wx-server-api/cgi/qrcode/index.js
+++ wx-server-api/cgi/qrcode/index.js
@@ -2,15 +2,15 @@ const axios = require("axios");
 
 module.exports = () => {
   return {
-    async create({ accessToken, expire_seconds, action_name, scene_id }) {
+    async create({ accessToken, expire_seconds, scene_str }) {
       const res = await axios.request({
         method: "POST",
         url: "https://api.weixin.qq.com/cgi-bin/qrcode/create",
         params: { access_token: accessToken },
         data: {
           expire_seconds: expire_seconds || 2592000,
-          action_name: action_name || "QR_SCENE",
-          action_info: { scene: { scene_id } },
+          action_name: "QR_STR_SCENE",
+          action_info: { scene: { scene_str } },
         },
       });
 
