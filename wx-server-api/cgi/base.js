f633fe7348350977557b80697df9bff8d13336f1
zhaojintian <729234283@qq.com>
2024-04-09 21:00:14 +0800

    update

--- wx-server-api/cgi/base.js
+++ wx-server-api/cgi/base.js
@@ -1,20 +1,31 @@
-const axios = require('axios')
+const axios = require("axios");
 
-module.exports = config => {
+module.exports = (config) => {
   return {
-    async getAccessToken () {
-      const { appId, secret } = config.mp || config.oa
+    async getAccessToken() {
+      const { appId, secret } = config.mp || config.oa;
       const { data } = await axios.request({
-        method: 'GET',
-        url: 'https://api.weixin.qq.com/cgi-bin/token',
+        method: "GET",
+        url: "https://api.weixin.qq.com/cgi-bin/token",
         params: {
-          grant_type: 'client_credential',
+          grant_type: "client_credential",
           appid: appId,
-          secret
-        }
-      })
+          secret,
+        },
+      });
 
-      return data
-    }
-  }
-}
+      return data;
+    },
+    async showQrCode({ ticket }) {
+      const { data } = await axios.request({
+        method: "GET",
+        url: "https://mp.weixin.qq.com/cgi-bin/showqrcode",
+        params: {
+          ticket,
+        },
+      });
+
+      return data;
+    },
+  };
+};
--- wx-server-api/cgi/qrcode/index.js
+++ wx-server-api/cgi/qrcode/index.js
@@ -2,14 +2,14 @@ const axios = require("axios");
 
 module.exports = () => {
   return {
-    async create({ accessToken, expire_seconds, scene_str }) {
+    async create({ accessToken, expire_seconds, action_name, scene_str }) {
       const res = await axios.request({
         method: "POST",
         url: "https://api.weixin.qq.com/cgi-bin/qrcode/create",
         params: { access_token: accessToken },
         data: {
           expire_seconds: expire_seconds || 2592000,
-          action_name: "QR_STR_SCENE",
+          action_name: action_name || "QR_STR_SCENE",
           action_info: { scene: { scene_str } },
         },
       });
